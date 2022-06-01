dd99349e24a844ae11f1516ca0c561e18848340e
shypotatosage <kathyjiegraal@gmail.com>
2022-06-01 15:36:04 +0700

    Tambah Splash Screen
    Mimi - Baru nambah splash screen, biar ada progress HAHAHAH

--- app/src/main/java/com/example/iscore/MainActivity.kt
+++ app/src/main/java/com/example/iscore/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.iscore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splash.animate().setDuration(3000).alpha(1f).withEndAction {
+            val a = Intent(this, RegisterActivity::class.java)
+            startActivity(a)
+            overridePendingTransition(R.anim.none, R.anim.slide_left_out)
+            finish()
+        }
     }
 }
