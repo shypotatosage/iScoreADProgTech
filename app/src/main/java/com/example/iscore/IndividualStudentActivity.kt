3a24ea35866f1c9f47404b0d4a5bce0be042ae6a
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2022-06-04 12:12:54 +0700

    30% jadi Individual Student + Tambahan Scroll View di StudentList

---
+++ app/src/main/java/com/example/iscore/IndividualStudentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iscore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndividualStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_student)
+    }
+}
