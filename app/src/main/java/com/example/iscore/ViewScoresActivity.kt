83f673989941dbb1ef754e26c320e29f2d5744fd
shypotatosage <kathyjiegraal@gmail.com>
2022-06-08 10:55:30 +0700

    ViewScores

---
+++ app/src/main/java/com/example/iscore/ViewScoresActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iscore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewScoresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_scores)
+    }
+}
