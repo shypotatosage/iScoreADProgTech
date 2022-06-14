43b7e2847ff7650935c1b21aa86f94ed11e55355
shypotatosage <kathyjiegraal@gmail.com>
2022-06-14 18:39:20 +0700

    StudentList

--- app/src/main/java/com/example/iscore/ClassListActivity.kt
+++ app/src/main/java/com/example/iscore/ClassListActivity.kt
@@ -17,6 +17,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_class_list.*
+import kotlinx.android.synthetic.main.classlist_card.*
 
 class ClassListActivity : AppCompatActivity(), CardListener {
 
@@ -63,7 +64,14 @@ class ClassListActivity : AppCompatActivity(), CardListener {
                         var classID = classSnapshot.child("id").getValue()
                         var imageUri = classSnapshot.child("imageUri").getValue()
 
+                        var i = 0
                         var students = arrayListOf<Student>()
+
+                        while (i < classSnapshot.child("students").childrenCount) {
+                            i++
+
+                            students.add(Student("a", "a", arrayListOf(), "a", "a"))
+                        }
 
                         var classroom = Classroom(classID.toString(),
                             className.toString(), classDesc.toString(), students)
@@ -75,8 +83,6 @@ class ClassListActivity : AppCompatActivity(), CardListener {
 
                     GlobalVar.classArrayList = classArrayList
                     classArrayList = arrayListOf()
-
-                    Toast.makeText(applicationContext, "" + classArrayList.size.toString(), Toast.LENGTH_LONG).show()
 
                     adapter.notifyDataSetChanged()
                 }
--- app/src/main/java/com/example/iscore/StudentListActivity.kt
+++ app/src/main/java/com/example/iscore/StudentListActivity.kt
@@ -1,25 +1,50 @@
 package com.example.iscore
 
+import Adapter.ClassListRVAdapter
+import Adapter.StudentListRVAdapter
+import Database.GlobalVar
+import Interface.CardListener
+import Model.Classroom
+import Model.Score
+import Model.Student
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_class_list.*
 import kotlinx.android.synthetic.main.activity_student_list.*
 
-class StudentListActivity : AppCompatActivity() {
+class StudentListActivity : AppCompatActivity(), CardListener {
 
-    private var position: Int = -1
+    private var classPosition: Int = -1
+    private var studentArrayList: ArrayList<Student> = arrayListOf()
+    private lateinit var adapter: StudentListRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_list)
 
         GetIntent()
+        setAdapter()
+        getData()
         listener()
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        setAdapter()
+        getData()
+    }
+
     private fun GetIntent() {
-        position  = intent.getIntExtra("Position", -1)
+        classPosition  = intent.getIntExtra("Position", -1)
     }
 
     private fun listener() {
@@ -29,10 +54,59 @@ class StudentListActivity : AppCompatActivity() {
 
         addStudentofclassFAB.setOnClickListener {
             val intent = Intent(this,AddStudentActivity::class.java).apply {
-                putExtra("Position", position)
+                putExtra("Position", classPosition)
             }
 
             startActivity(intent)
         }
+    }
+
+    private fun getData() {
+        val database = Firebase.database
+        val ref = database.getReference("users").child("users").child(GlobalVar.user.id).child("classes").child(GlobalVar.classArrayList[classPosition].id).child("students")
+
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (classSnapshot in snapshot.children) {
+                        var studentName = classSnapshot.child("name").getValue()
+                        var studentAddress = classSnapshot.child("address").getValue()
+                        var studentID = classSnapshot.child("id").getValue()
+                        var studentNumber = classSnapshot.child("phoneNumber").getValue()
+
+                        var scores = arrayListOf<Score>()
+
+                        var student = Student(studentID.toString(), studentName.toString(), scores, studentAddress.toString(), studentNumber.toString())
+
+                        studentArrayList.add(student!!)
+                    }
+
+                    GlobalVar.classArrayList[classPosition].students = studentArrayList
+                    studentArrayList = arrayListOf()
+
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun setAdapter() {
+        adapter = StudentListRVAdapter(studentArrayList, this)
+        studentList_recyclerView.layoutManager = LinearLayoutManager(this)
+        studentList_recyclerView.adapter = adapter
+    }
+
+    override fun onCardClick(position: Int) {
+        val myIntent = Intent(this, StudentListActivity::class.java).apply {
+            putExtra("Class Position", classPosition)
+            putExtra("Student Position", position)
+        }
+
+        startActivity(myIntent)
     }
 }
